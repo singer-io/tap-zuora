745afaaec257185f8462cf09c087b5a705c08231
Tim Visher <tim@stitchdata.com>
2017-05-24 13:37:42 -0400

    0.0.2 → 0.0.3
    Motivation
    ----------
    
    API changes
    -----------
    
    Implementation Notes
    --------------------
    
    Functional Tests
    ----------------
    
    Regression Tests
    ----------------
    
    Screenshots
    -----------

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-zuora',
-      version='0.0.2',
+      version='0.0.3',
       description='Singer.io tap for extracting data from the Zuora API',
       author='Stitch',
       url='https://singer.io',
