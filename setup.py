c7ea80ffa0db6db57cf5037b705dda1a9ebc1b06
Tim Visher <tim@stitchdata.com>
2017-05-31 15:47:09 -0400

    0.0.5 → 0.0.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-zuora',
-      version='0.0.5',
+      version='0.0.6',
       description='Singer.io tap for extracting data from the Zuora API',
       author='Stitch',
       url='https://singer.io',
