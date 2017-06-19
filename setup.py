de57792943da18c657a1a3d34172bc60d8aa4f82
Paul Santa Clara <psantaclara@rjmetrics.com>
2017-06-19 14:39:52 -0400

    bumping version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-zuora',
-      version='0.0.7',
+      version='0.0.8',
       description='Singer.io tap for extracting data from the Zuora API',
       author='Stitch',
       url='https://singer.io',
