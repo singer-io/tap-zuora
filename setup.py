e2dedf1f6035c36ede398417eac2be022c946610
Andy Lu <andy@stitchdata.com>
2019-06-14 18:05:44 +0000

    Bump to v1.1.10

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-zuora',
-      version='1.1.9',
+      version='1.1.10',
       description='Singer.io tap for extracting data from the Zuora API',
       author='Stitch',
       url='https://singer.io',
