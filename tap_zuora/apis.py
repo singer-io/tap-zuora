0c5f8278f9d5db5692b12aec1e267c4ab408a0a5
kethan1122 <kcherukuri@talend.com>
2022-10-06 21:17:25 +0530

    type hinting for apis.py file

--- tap_zuora/apis.py
+++ tap_zuora/apis.py
@@ -2,6 +2,7 @@ import pendulum
 import singer
 from singer import metadata
 from tap_zuora.exceptions import ApiException
+from typing import List, Dict, Union, Optional
 
 MAX_EXPORT_DAYS = 30
 SYNTAX_ERROR = "There is a syntax error in one of the queries in the AQuA input"
@@ -11,19 +12,19 @@ NO_DELETED_SUPPORT = ("Objects included in the queries do not support the queryi
 LOGGER = singer.get_logger()
 
 
-def selected_fields(stream):
-    mdata = metadata.to_map(stream['metadata'])
+def selected_fields(stream: Dict) -> List:
+    mdata = metadata.to_map(stream["metadata"])
     fields = [f for f, s in stream["schema"]["properties"].items()
-              if metadata.get(mdata, ('properties', f), 'selected')
-              or metadata.get(mdata, ('properties', f), 'inclusion') == 'automatic']
+              if metadata.get(mdata, ("properties", f), "selected")
+              or metadata.get(mdata, ("properties", f), "inclusion") == "automatic"]
 
     # Remove Deleted from the query if its selected
-    if 'Deleted' in fields:
-        fields.remove('Deleted')
+    if "Deleted" in fields:
+        fields.remove("Deleted")
     return fields
 
 
-def joined_fields(fields, stream):
+def joined_fields(fields: List, stream: Dict) -> List:
     mdata = metadata.to_map(stream['metadata'])
     joined_fields_list = []
     for field_name in fields:
@@ -35,7 +36,7 @@ def joined_fields(fields, stream):
     return joined_fields_list
 
 
-def format_datetime_zoql(datetime_str, date_format):
+def format_datetime_zoql(datetime_str: str, date_format: str):
     return pendulum.parse(datetime_str, tz=pendulum.timezone("UTC")).strftime(date_format)
 
 
@@ -44,7 +45,7 @@ class ExportFailed(Exception):
 
 
 class ExportTimedOut(ExportFailed):
-    def __init__(self, timeout, unit):
+    def __init__(self, timeout: int, unit: str):
         super().__init__(f"Export failed (TimedOut): The job took longer than {timeout} {unit}")
 
 
@@ -77,7 +78,7 @@ class Aqua:
     ]
 
     @staticmethod
-    def make_payload(stream_name, project, query, partner_id, deleted=False):
+    def make_payload(project: str, query: str, partner_id: str, deleted: Optional[bool] = False) -> Dict:
         # NB - 4/5/19 - Were told by zuora support to use the same value
         # for both project and name to imply an incremental export
         rtn = {
@@ -104,7 +105,7 @@ class Aqua:
         return rtn
 
     @staticmethod
-    def deleted_records_available(stream):
+    def deleted_records_available(stream: Dict) -> Union[str, bool]:
         if stream['tap_stream_id'] in Aqua.DOES_NOT_SUPPORT_DELETED:
             LOGGER.info(f"Deleted fields are not supported for stream - {stream['tap_stream_id']}."
                         f" Not selecting deleted records.")
@@ -114,7 +115,7 @@ class Aqua:
         return "Deleted" in stream["schema"]["properties"] and metadata.get(mdata, ('properties', 'Deleted'), 'selected')
 
     @staticmethod
-    def get_query(state, stream):
+    def get_query(stream: Dict) -> str:
         selected_field_names = selected_fields(stream)
         dotted_field_names = joined_fields(selected_field_names, stream)
         fields = ", ".join(dotted_field_names)
@@ -126,13 +127,13 @@ class Aqua:
         return query
 
     @staticmethod
-    def get_payload(state, stream, partner_id):
+    def get_payload(state: Dict, stream: Dict, partner_id: str) -> Dict:
         stream_name = stream["tap_stream_id"]
         version = state["bookmarks"][stream["tap_stream_id"]]["version"]
         project = f"{stream_name}_{version}"
-        query = Aqua.get_query(state, stream)
+        query = Aqua.get_query(stream)
         deleted = Aqua.deleted_records_available(stream)
-        payload = Aqua.make_payload(stream_name, project, query, partner_id, deleted)
+        payload = Aqua.make_payload(project, query, partner_id, deleted)
 
         if stream.get("replication_key"):
             # Incremental time must be in Pacific time
@@ -145,7 +146,7 @@ class Aqua:
         return payload
 
     @staticmethod
-    def create_job(client, state, stream):
+    def create_job(client, state: Dict, stream: Dict) -> str:
         endpoint = "v1/batch-query/"
         # This _always_ submits with an incremental_time which I think
         # means that we're never executing a full export which means we
@@ -171,7 +172,7 @@ class Aqua:
         return resp["id"]
 
     @staticmethod
-    def stream_status(client, stream_name):
+    def stream_status(client, stream_name: str) -> str:
         """
         Check if the provided Zuora object (stream_name) can be queried via
         AQuA API by issuing a small export job of 1 row. This job must be
@@ -183,7 +184,7 @@ class Aqua:
         """
         endpoint = "v1/batch-query/"
         query = f"select * from {stream_name} limit 1"
-        payload = Aqua.make_payload(stream_name, "discover", query, client.partner_id)
+        payload = Aqua.make_payload("discover", query, client.partner_id)
         resp = client.aqua_request("POST", endpoint, json=payload).json()
 
         # Cancel this job to keep concurrency low.
@@ -200,7 +201,7 @@ class Aqua:
 
     # Must match call signature of other APIs
     @staticmethod
-    def job_ready(client, job_id):
+    def job_ready(client, job_id: str) -> bool:
         endpoint = f"v1/batch-query/jobs/{job_id}"
         data = client.aqua_request("GET", endpoint).json()
         if data["status"] == "completed":
@@ -212,7 +213,7 @@ class Aqua:
 
     # Must match call signature of other APIs
     @staticmethod
-    def get_file_ids(client, job_id):
+    def get_file_ids(client, job_id: str) -> List:
         endpoint = f"v1/batch-query/jobs/{job_id}"
         data = client.aqua_request("GET", endpoint).json()
         if "segments" in data["batches"][0]:
@@ -221,7 +222,7 @@ class Aqua:
 
     # Must match call signature of other APIs
     @staticmethod
-    def stream_file(client, file_id):
+    def stream_file(client, file_id: str):
         endpoint = f"v1/file/{file_id}"
         return client.aqua_request("GET", endpoint, stream=True).iter_lines()
 
@@ -230,14 +231,14 @@ class Rest:
     ZOQL_DATE_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
 
     @staticmethod
-    def make_payload(query):
+    def make_payload(query: str) -> Dict:
         return {
             "Format": "csv",
             "Query": query,
         }
 
     @staticmethod
-    def get_query(stream, start_date, end_date):
+    def get_query(stream: Dict, start_date: Union[str, None], end_date: Union[str, None]) -> str:
         selected_field_names = selected_fields(stream)
         dotted_field_names = joined_fields(selected_field_names, stream)
         fields = ", ".join(dotted_field_names)
@@ -253,12 +254,12 @@ class Rest:
         return query
 
     @staticmethod
-    def get_payload(stream, start_date, end_date):
+    def get_payload(stream: Dict, start_date: Union[str, None], end_date: Union[str, None]) -> Dict:
         query = Rest.get_query(stream, start_date, end_date)
         return Rest.make_payload(query)
 
     @staticmethod
-    def create_job(client, stream, start_date=None, end_date=None):
+    def create_job(client, stream: Dict, start_date: Union[str, None] = None, end_date: Union[str, None] = None) -> str:
         endpoint = "v1/object/export"
         payload = Rest.get_payload(stream, start_date, end_date)
         resp = client.rest_request("POST", endpoint, json=payload).json()
@@ -266,7 +267,7 @@ class Rest:
 
     # Must match call signature of other APIs
     @staticmethod
-    def job_ready(client, job_id):
+    def job_ready(client, job_id: str) -> bool:
         LOGGER.info(f"job details: Id.{job_id}")
         endpoint = f"v1/object/export/{job_id}"
         data = client.rest_request("GET", endpoint).json()
@@ -279,19 +280,19 @@ class Rest:
 
     # Must match call signature of other APIs
     @staticmethod
-    def get_file_ids(client, job_id):
+    def get_file_ids(client, job_id: str) -> List:
         endpoint = f"v1/object/export/{job_id}"
         data = client.rest_request("GET", endpoint).json()
         return [data["FileId"]]
 
     # Must match call signature of other APIs
     @staticmethod
-    def stream_file(client, file_id):
+    def stream_file(client, file_id: str):
         endpoint = f"v1/files/{file_id}"
         return client.rest_request("GET", endpoint, stream=True).iter_lines()
 
     @staticmethod
-    def stream_status(client, stream_name):
+    def stream_status(client, stream_name: str) -> str:
         endpoint = "v1/object/export"
         query = f"select * from {stream_name} limit 1"
         payload = {
