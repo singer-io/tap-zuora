41dbb21590d3ab583f79ea8ce330f7726c73c9c2
kethan1122 <kcherukuri@talend.com>
2022-10-13 09:21:25 +0530

    catalog type for legacy state

--- tap_zuora/__init__.py
+++ tap_zuora/__init__.py
@@ -3,12 +3,11 @@ import sys
 import time
 
 import singer
+from singer import Catalog, metadata
 
-from singer import metadata, Catalog
 from tap_zuora.client import Client
 from tap_zuora.discover import discover_streams
 from tap_zuora.sync import sync_stream
-
 
 REQUIRED_CONFIG_KEYS = [
     "start_date",
@@ -22,19 +21,31 @@ LOGGER = singer.get_logger()
 
 
 def convert_legacy_state(catalog: Catalog, state: dict) -> dict:
+    """creates a legacy state file this method gets invoked when there is no
+    bookmarks key in non-empty state file."""
     new_state = {"bookmarks": {}, "current_stream": state.get("current_stream")}
-    for stream in catalog["streams"]:
-        if stream.get("selected") and stream.get("replication_key") and stream["tap_stream_id"] in state:
-            new_state["bookmarks"][stream["tap_stream_id"]][stream["replication_key"]] = state[stream["tap_stream_id"]]
+    for stream in catalog.streams:
+        if (
+            stream.is_selected()
+            and stream.replication_key
+            and stream.tap_stream_id in state
+        ):
+            new_state["bookmarks"][stream["tap_stream_id"]][
+                stream["replication_key"]
+            ] = state[stream["tap_stream_id"]]
 
     return new_state
 
 
 def stream_is_selected(mdata: dict) -> bool:
-    return mdata.get((), {}).get('selected', False)
+    """check whether a stream is selected returns boolean value."""
+    return mdata.get((), {}).get("selected", False)
 
 
 def validate_state(config: dict, catalog: Catalog, state: dict) -> dict:
+    """Validates the state file Sets the bookmark value for each selected
+    stream as start_date if bookmark value is None or not available Sets the
+    current_stream to None if the current_stream is not selected."""
     if "bookmarks" not in state:
         if state.keys():
             LOGGER.info("Legacy state detected")
@@ -55,21 +66,34 @@ def validate_state(config: dict, catalog: Catalog, state: dict) -> dict:
 
         if stream.tap_stream_id not in state["bookmarks"]:
             LOGGER.info("Initializing state for %s", stream.tap_stream_id)
-            version = int(time.time())
-            state["bookmarks"][stream.tap_stream_id] = {"version": version}
+            singer.write_bookmark(
+                state, stream.tap_stream_id, "version", int(time.time())
+            )
 
         if not stream.replication_key:
             continue
 
-        if stream.replication_key not in state["bookmarks"][stream.tap_stream_id] or \
-           state["bookmarks"][stream.tap_stream_id][stream.replication_key] is None:
-            LOGGER.info("Setting start date for %s to %s", stream.tap_stream_id, config["start_date"])
-            state["bookmarks"][stream.tap_stream_id][stream.replication_key] = config["start_date"]
+        if (
+            stream.replication_key not in state["bookmarks"][stream.tap_stream_id]
+            or state["bookmarks"][stream.tap_stream_id][stream.replication_key] is None
+        ):
+            LOGGER.info(
+                "Setting start date for %s to %s",
+                stream.tap_stream_id,
+                config["start_date"],
+            )
+            singer.write_bookmark(
+                state,
+                stream.tap_stream_id,
+                stream.replication_key,
+                config["start_date"],
+            )
 
     return state
 
 
 def do_discover(client: Client, force_rest: bool = False):
+    """starts the Discover process."""
     LOGGER.info("Starting discover")
     catalog = {"streams": discover_streams(client, force_rest)}
     json.dump(catalog, sys.stdout, indent=2)
@@ -77,6 +101,7 @@ def do_discover(client: Client, force_rest: bool = False):
 
 
 def do_sync(client: Client, catalog: Catalog, state: dict, force_rest: bool = False):
+    """Starts the sync process for all the selected streams."""
     starting_stream = state.get("current_stream")
     if starting_stream:
         LOGGER.info("Resuming sync from %s", starting_stream)
@@ -122,10 +147,16 @@ def main():
     if not force_rest:
         partner_id = args.config.get("partner_id")
         if not partner_id:
-            raise Exception("Config is missing required `partner_id` key when using the AQuA API")
+            raise Exception(
+                "Config is missing required `partner_id` key when using the AQuA API"
+            )
 
     if args.discover:
         do_discover(client, force_rest)
     elif args.catalog:
         state = validate_state(args.config, args.catalog, args.state)
         do_sync(client, args.catalog, state, force_rest)
+
+
+if __name__ == "__main__":
+    main()
