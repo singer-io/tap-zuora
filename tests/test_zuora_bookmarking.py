cae9133aaa1cdc0c9341741780c58e3417740759
“rdeshmukh15” <redeshmukh@talend.com>
2022-11-18 09:39:46 +0000

    docstring change

--- tests/test_zuora_bookmarking.py
+++ tests/test_zuora_bookmarking.py
@@ -15,20 +15,22 @@ class ZuoraBookmarking(ZuoraBaseTest):
         self.run_test("REST")
 
     def run_test(self, api_type):
-        """Verify that for each stream you can do a sync which records
+        """
+        - Verify that for each stream you can do a sync which records
         bookmarks. That the bookmark is the maximum value sent to the target
-        for the replication key that second sync respects the bookmark data of
-        the second sync is >= the bookmark from the first sync. The number of
-        records in the 2nd sync is less than the first (This assumes that new
-        data added to the stream is done at a rate slow enough that you haven't
-        doubled the amount of data from the start date to the first sync
-        between the first sync and second sync run in this test)
+        for the replication key.
+        - A second sync respects the bookmark
+        - All data of the second sync is >= the bookmark from the first sync.
+        - The number of records in the 2nd sync is less then the first
+        (This assumes that new data added to the stream is done at a rate slow enough that
+        you haven't doubled the amount of data from the start date to the first
+        sync between the first sync and second sync run in this test)
+        - Verify that for full table stream, all data replicated in sync 1 is replicated
+        again in sync 2.
 
-        Verify that for full table stream, all data replicated in sync 1 is replicated again in sync 2.
-
-        PREREQUISITE
-        For EACH stream that is incrementally replicated there are multiple rows of data with
-            different values for the replication key
+        PREREQUISITE For EACH stream that is incrementally replicated
+        there are multiple rows of data with different values for
+        the replication key
         """
         self.zuora_api_type = api_type
 
